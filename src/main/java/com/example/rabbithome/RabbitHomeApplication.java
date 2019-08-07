f9bf8a858bafd32d27b64786523f58812954f5a2
hansol <onesorry49@gmail.com>
2019-08-07 21:40:02 +0900

    first spring boot project

---
+++ src/main/java/com/example/rabbithome/RabbitHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbithome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitHomeApplication.class, args);
+	}
+
+}
